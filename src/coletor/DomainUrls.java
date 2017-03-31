c245bcc87ebe6b854604a7ba38c86dadcf2c75e2
Chen <dehua.chen@sga.pucminas.br>
2017-03-31 18:18:52 -0300

    Created domain wrapper: DomainUrls.

---
+++ src/coletor/DomainUrls.java
@@ -0,0 +1,28 @@
+package coletor;
+
+import java.util.ArrayList;
+
+public class DomainUrls {
+	private String domain;
+	private ArrayList<String> urls;
+	
+	public DomainUrls(String domain, ArrayList<String> urls) {
+		this.domain = domain;
+		this.urls = urls;
+	}
+	
+	public String getDomain() {
+		return domain;
+	}
+	public void setDomain(String domain) {
+		this.domain = domain;
+	}
+	public ArrayList<String> getUrls() {
+		return urls;
+	}
+	public void setUrls(ArrayList<String> urls) {
+		this.urls = urls;
+	}
+	
+	
+}
