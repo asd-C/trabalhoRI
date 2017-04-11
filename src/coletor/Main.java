6a9735536f8eaceeb05047c6a8be528cd03d744d
Chen <dehua.chen@sga.pucminas.br>
2017-04-10 22:11:59 -0300

    Added Timer.

--- src/coletor/Main.java
+++ src/coletor/Main.java
@@ -1,18 +1,40 @@
 package coletor;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.jsoup.nodes.Document;
 
+import edu.stanford.nlp.ie.AbstractSequenceClassifier;
+import edu.stanford.nlp.ie.crf.CRFClassifier;
+import edu.stanford.nlp.ling.CoreAnnotations.AnswerAnnotation;
+import edu.stanford.nlp.ling.CoreLabel;
 import entity.Seeds;
+import utils.Timer;
 import utils.Writer;
 
 public class Main {
+	
+	private static final String MODEL_PATH = "./lib/classifier/english.all.3class.distsim.crf.ser.gz";
 
+	public static AbstractSequenceClassifier<CoreLabel> prepareClassifier() {
+		String model = MODEL_PATH;
+		AbstractSequenceClassifier<CoreLabel> classifier = null;
+			
+			try {
+				classifier = CRFClassifier.getClassifier(model);
+			} catch (ClassCastException e) { e.printStackTrace(); } 
+			catch (ClassNotFoundException e) { e.printStackTrace(); } 
+			catch (IOException e) { e.printStackTrace(); }
+		
+		return classifier;
+	}
+	
 	public static void coletor() {
 		HashMap<String, Document> documents;
 		Set<String> seeds;
@@ -28,7 +50,8 @@ public class Main {
 		Fetcher fetcher = new Fetcher();
 		Parser parser = new Parser();
 		Scheduler scheduler = new Scheduler();
-		Writer writer = new Writer("./output.txt");
+		Timer timer = new Timer();
+//		Writer writer = new Writer("./output.txt");
 		
 		scheduler.addNewUrls(new HashSet<String>(Arrays.asList(urls)));
 		
@@ -37,18 +60,25 @@ public class Main {
 			
 //			Seeds.showUnvisitedSeeds();
 			
-			System.out.println("\nRound: " + round);
-			
+			System.out.println("\n-------------------- Round: " + round + " --------------------\n");
+			timer.startTimer(Timer.FILTERURLS);
 			new_seeds = FilterUrls.filtrar(scheduler.generateNewSeeds());
+			timer.finishTimer(Timer.FILTERURLS);
 			System.out.println("Number of seeds: " + new_seeds.size());
 			
+			timer.startTimer(Timer.FETCHER);
 			documents = fetcher.fetchAll(new_seeds);
+			timer.finishTimer(Timer.FETCHER);
 			System.out.println("Number of documents: " + documents.size());
 			
+			timer.startTimer(Timer.PARSER);
 			seeds = parser.getURLsFromPages(documents);
+			timer.finishTimer(Timer.PARSER);
 			System.out.println("Number of new seeds: " + seeds.size());
 			
-			writer.saveTo(writer.format(parser.getTextsFromPages(documents)));
+//			parser.getTextsFromPages(documents);
+			
+//			writer.saveTo(writer.format(parser.getTextsFromPages(documents)));
 			
 			scheduler.addNewUrls(seeds);
 			
@@ -57,11 +87,13 @@ public class Main {
 			
 			Seeds.showVisitedSeeds();
 			
+			System.out.println("\n-------------------- End of Round " + round + " --------------------\n");
 			round++;
 		}
 	}
 	
 	public static void main(String... args) {
+//		prepareClassifier();
 		coletor();
 //		try {
 //			new Parser().removeAnchor("http://en.wikipedia.org/wiki/Barack_obama");
---
+++ src/utils/Timer.java
@@ -0,0 +1,47 @@
+package utils;
+
+import java.util.HashMap;
+
+public class Timer {
+	private HashMap<Integer, Long> timers;
+	public static final int FETCHER = 0;
+	public static final int FILTERURLS = 1;
+	public static final int PARSER = 2;
+	public static final int SCHEDULER = 3;
+	public static final int WRITER = 4;
+	public static final int READER = 5;
+	
+	private static final String[] labels = {
+			"Fetcher", 
+			"FilterUrls", 
+			"Parser", 
+			"Scheduler", 
+			"Writer", 
+			"Reader"};
+	
+	public Timer() {
+		timers = new HashMap<Integer, Long>();
+	}
+	
+	public void startTimer(int which) {
+		timers.put(which, System.currentTimeMillis());
+		info("Starting " + labels[which] + " timer...");
+	}
+	
+	public long finishTimer(int which) {
+		Long current = System.currentTimeMillis();
+		Long l = timers.get(which);
+		
+		if (l == null) {
+			info("Had not started" + labels[which] + " timer...");
+			return -1;
+		} else {
+			info(labels[which] + " spent " + (current-l) + " ms.");
+			return current - l;
+		}
+	}
+	
+	public void info(String msg) {
+		System.out.println(msg);
+	}
+}
